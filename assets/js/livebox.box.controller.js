38698b84240106765737167b7b7708d52b2b58fd
piznel <piznel@hotmail.com>
2018-11-11 15:45:12 +0100

    Box Livebox init

---
+++ assets/js/livebox.box.controller.js
@@ -0,0 +1,36 @@
+(function () {
+	'use strict';
+	
+	angular
+		.module('gladys')
+		.controller('liveboxCtrl', liveboxCtrl); 
+		//Gardez bien le "BoxCtrl" !
+
+        liveboxCtrl.$inject  = ['boxService'];
+
+	function  liveboxCtrl(boxService) {
+		var  vm  =  this;
+		vm.init = init;
+        vm.updateBoxParams = updateBoxParams;
+		vm.box = null;
+		
+		function init(id){
+			boxService.getById(id) //On appel la route "getById" du service
+				.then(function(data){
+					vm.box = data.data; //On sauvegarde tout 
+					activate(); //On active notre box
+				});
+		}
+		
+		function activate(){
+			console.log(vm.box.param)
+		}
+
+		function updateBoxParams(hello_world){
+			boxService.update(vm.box.id, {params: {text: hello_world}})
+				.then(function(data){
+					console.log("Paramètres sauvegardés !")
+				});
+		}
+	}
+})();
